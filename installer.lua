255136856055558ee9b6775b4abaf18474d099f4
Swedz <me@swedz.net>
2023-09-04 20:31:21 -0400

    Make sure installer deletes old files first

--- installer.lua
+++ installer.lua
@@ -1,3 +1,7 @@
+shell.execute("delete", "vault_config")
+shell.execute("delete", "vault_core")
+shell.execute("delete", "vault_items")
+shell.execute("delete", "vault_interface")
 shell.execute("wget", "https://raw.githubusercontent.com/Swedz/Vault-Storage/master/vault_config.lua", "vault_config")
 shell.execute("wget", "https://raw.githubusercontent.com/Swedz/Vault-Storage/master/vault_core.lua", "vault_core")
 shell.execute("wget", "https://raw.githubusercontent.com/Swedz/Vault-Storage/master/vault_items.lua", "vault_items")
