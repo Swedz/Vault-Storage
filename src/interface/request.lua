deca4067fdd3056b541466a94c9fdd174bd86a1b
Swedz <me@swedz.net>
2023-09-05 21:38:22 -0400

    Remove unused turtle inventory event code

--- src/interface/request.lua
+++ src/interface/request.lua
@@ -94,8 +94,6 @@ function handleRequestScreen()
                     handleTyping(eventData[2])
                 elseif event == "key" then
                     handleKeyPress(eventData[2])
-                elseif event == "turtle_inventory" then
-                    handleTurtleInventory()
                 elseif event == "term_resize" then
                     updateTermSize()
                     updateRequestTermSize()
